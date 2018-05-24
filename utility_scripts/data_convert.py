57548d01f76783aa4c9b533399370bb09710672e
tanay g <tanay.gahlot@innoplexus.com>
2018-05-24 19:45:29 +0530

    code review for data dump mechanism

--- utility_scripts/data_convert.py
+++ utility_scripts/data_convert.py
@@ -1,4 +1,4 @@
-""" converting the json data to tensorflow model feeding data"""
+"""converting the json data to tensorflow model feeding data"""
 
 #import dependencies
 import collections
@@ -38,27 +38,29 @@ tf.app.flags.DEFINE_integer('max_words', 500000, 'Define the max number of words
 
 def _get_json_file_data():
     """ reads the json file and returns the loaded data"""
+    loaded_data = []
     with open(FLAGS.in_file, mode='r') as file_json:
         loaded_data = json.load(file_json)
     return loaded_data
 
-
 def _text_to_vocabulary(output_file):
     """Converts the training corpus text to vocabulary
        and stores it in vocab file"""
+    # loading data.
     loaded_data = _get_json_file_data()
-
+    # get all the words in the counter object.
     counter = collections.Counter()
-
     for document in loaded_data:
         body = document['body']
         abstract = document['abstract']
+        # getting words.
+        words_body = body.split(" ")
+        words_abstract = abstract.split(" ")
+        # updating the counter.
+        counter.update(words_body)
+        counter.update(words_abstract)
 
-        words = body.split()
-        counter.update(words)
-        words = abstract.split()
-        counter.update(words)
-
+    # write the vocab in the file.
     with open(output_file, mode='w') as file_vocab:
         for word, count in counter.most_common(FLAGS.max_words):
             file_vocab.write(word + ' ' + str(count) + '\n')
@@ -66,7 +68,6 @@ def _text_to_vocabulary(output_file):
         file_vocab.write('</s> 0\n')
         file_vocab.write('<UNK> 0\n')
         file_vocab.write('<PAD> 0\n')
-
 
 def _text_to_binary(output_files, split_fractions):
     """ Splitting the input data by split fractions
@@ -78,41 +79,44 @@ def _text_to_binary(output_files, split_fractions):
 
     start_from_index = 0
     for index, file_out in enumerate(output_files):
+        # calculating no of examples in given file.
         sample_count = int(len(loaded_data) * split_fractions[index])
         print(file_out + ": " + str(sample_count))
-
+        # computing corresponding index.
         end_index = min(start_from_index + sample_count, len(loaded_data))
+        # converting the block to binary.
         _convert_json_to_binary(loaded_data[start_from_index:end_index], file_out)
-
+        # updating index.
         start_from_index = end_index
-
 
 def _convert_json_to_binary(input_text_files, output_filename):
     """ converting the split sections of json filenames to
         binary for training and storing in output file """
     with open(output_filename, 'wb') as out_file:
         for document in input_text_files:
+            # extracting body and abstract out of document.
             body = document['body']
             abstract = document['abstract']
 
-            body = body.replace('\n', ' ').replace('\t', ' ')
-            abstract = abstract.replace('\n', ' ').replace('\t', ' ')
-
+            #TODO: Change it with re.escape()
             for ch in ["\'"]:
                 if ch in body:
                     body = body.replace(ch, "\\"+ch)
 
+            #TODO: Change it with re.escape()
             for ch in ["\'"]:
                 if ch in abstract:
                     abstract = abstract.replace(ch, "\\"+ch)
-
+            # sentence tokeization of body.
             sentences = sent_tokenize(body)
-            body = '<d><p>' + ' '.join(['<s>' + sentence + '</s>' for sentence in sentences]) + '</p></d>'
+            body = '<d><p>' + ' '.join(['<s>' + sentence + '</s>' for sentence\
+                                        in sentences]) + '</p></d>'
+            # encoding in unicode.
             body = body.encode('utf8')
-
+            # encoding abstract.
             abstract = '<d><p><s>' + abstract + ' </s></p></d>'
             abstract = abstract.encode('utf8')
-
+            # converting abstract and body to tf example(binary format!).
             tf_example = example_pb2.Example()
             tf_example.features.feature['article'].bytes_list.value.extend([body])
             tf_example.features.feature['abstract'].bytes_list.value.extend([abstract])
@@ -121,26 +125,21 @@ def _convert_json_to_binary(input_text_files, output_filename):
             out_file.write(struct.pack('q', str_len))
             out_file.write(struct.pack('%ds' % str_len, tf_example_str))
 
-
 def main(unused_argv):
-
     """ the main function """
     assert FLAGS.command and FLAGS.in_file and FLAGS.out_files
+    # parsing command line variables.
     output_filenames = FLAGS.out_files.split(',')
+    #TODO: isse usse karo be! @@&#^&
     input_file = FLAGS.in_file
 
     if FLAGS.command == 'text_to_binary':
         assert FLAGS.split
-
         split_fractions = [float(s) for s in FLAGS.split.split(',')]
-
         assert len(output_filenames) == len(split_fractions)
-
         _text_to_binary(output_filenames, split_fractions)
-
     elif FLAGS.command == 'text_to_vocabulary':
         assert len(output_filenames) == 1
-
         _text_to_vocabulary(output_filenames[0])
 
 
