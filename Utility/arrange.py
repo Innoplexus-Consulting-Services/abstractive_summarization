92f2eff2688587b9841adf1c80092a67e409c222
abhiverma3511 <verma.abhishek9165@innoplexus.com>
2018-06-06 20:15:08 +0530

    Added Rouge Score

--- Utility/arrange.py
+++ Utility/arrange.py
@@ -2,11 +2,14 @@
     run in decode mode."""
 import sys
 import os
+from pythonrouge.pythonrouge import Pythonrouge
+import numpy as np
 
 #decode,ref and arranged_output filename
 decoded_file = 'decode'
 reference_file = 'reference'
 arranged_file = 'decode_sample'
+Fsum = []
 
 count = 0
 
@@ -39,7 +42,21 @@ def _read_and_arrange_write(ref,dec,arr):
                 f.write(ref_sum[index])
                 f.write('\n*********GENERATED(' + str(index) +')******\n')
                 f.write(dec_lines[index])
+                ref_text = [[[ref_sum[index]]]]
+                summary_text = [[dec_lines[index]]]
+                rouge = Pythonrouge(summary_file_exist=False,
+                    summary=summary_text, reference=ref_text,
+                    n_gram=1, ROUGE_SU4=False, ROUGE_L=False,
+                    recall_only=True, stemming=True, stopwords=True,
+                    word_level=True,
+                    use_cf=False, cf=95, scoring_formula='average',
+                    favor=True, p=0.5)
+                score = rouge.calc.score()
+                Fsum += score['Rouge1-R']
+                print(score)
                 f.write('-`-----------------------------------------------------------------------------------------------------------')
+
+        print("Average Rouge Score : " + str(np.mean(Fsum)))
 
 if __name__ = '__main__':
     _read_and_arrange_write(reference_file,decoded_file,arranged_file)
